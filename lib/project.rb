935b974c2a5a9dc32a558747eab9ae718a6076d4
Ian Marrs <ian.t.marrs@gmail.com>
2020-02-05 22:10:15 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -9,7 +9,7 @@ class Project
   def add_backer(backer)
     if backer.instance_of? Backer
       self.backers << backer
-      @backers << self
+      project.backed_projects << self
     end
   end
 
